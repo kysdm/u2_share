a5035c1d558c7a1f858a5fc42c32c979c1a07c29
kysdm <gxk7231@gmail.com>
2020-12-06 11:53:49 +0800

    支持个人主页内的种子列表

--- u2share_gd.user.js
+++ u2share_gd.user.js
@@ -1,57 +1,66 @@
 // ==UserScript==
 // @name         U2种子备份查询
 // @namespace    https://u2.dmhy.org/
-// @version      1.9
+// @version      2.0
 // @description  在页面下载旁加入图标，支持一键发送请求。
 // @author       McHobby & kysdm
-// @grant        GM_setValue
-// @grant        GM_getValue
-// @grant        GM_xmlhttpRequest
+// @grant        none
 // @match        *://u2.dmhy.org/torrents.php*
-// @match        *://u2.dmhy.org/sendmessage.php*
-// @match        *://u2.dmhy.org/details.php*
-// @match        *://u2.dmhy.org/offers.php*
+// @match        *://u2.dmhy.org/sendmessage.php?receiver=*
+// @match        *://u2.dmhy.org/details.php?id=*
+// @match        *://u2.dmhy.org/offers.php?id=*
+// @match        *://u2.dmhy.org/userdetails.php?id=*
 // @require      https://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js
+// @require      https://unpkg.com/xhook@latest/dist/xhook.min.js
 // @connect      cdn.jsdelivr.net
 // ==/UserScript==
 
-(async ($) => {
+// $.cookie 方法感觉可以弃用了，直接用 localStorage 就可以
+// 在用户界面发起的请求，由于列表里没有做种人数，请求不一定会允许
+(function () {
     'use strict';
 
-    const gdListUrl = 'https://cdn.jsdelivr.net/gh/kysdm/u2_share@main/u2list.txt';
+    const userid = 45940; // 勿动
+    const Uploaders = 7;
+    let GstaticIco;
     const date = new Date().getDate();
-    let gdListObj = GM_getValue('u2_gd_list', null);
-    if (gdListObj === null || gdListObj.date !== date) {
-        await new Promise((resolve, reject) => {
-            GM_xmlhttpRequest({
-                method: 'GET',
-                url: gdListUrl,
-                onload: r => r.status < 400 ? resolve(r.responseText) : reject(r.status),
-                onerror: r => reject(r.status)
-            })
-        })
-            .then(g => {
-                gdListObj = { date: date, list: g.trim().split('\n') };
-                GM_setValue('u2_gd_list', gdListObj);
-            })
-            .catch(e => {
-                console.log('下载列表发生错误，HTTP状态码[' + e + ']。');
-            })
-    }
 
+    // Ajax-hook 有点复杂，用 xhook 简单点...
+    // 好像 Ajax-hook 的处理效率更高? 也可能我的写法有问题
+    // 列表如果上千了，就可以明显感觉到慢了，chrome任务管理器里直接99占用，wwwwwww
+    // 如果其他脚本也劫持了 ajax 请求，那可能会发生冲突!!!!!!!!
+    xhook.after(function (request, response) {
+        if (request.url.match(/getusertorrentlistajax\.php\?userid=\d+?&type=/i)) {
+            response.text = userdetails(response.text);
+        }
+    });
+
+    let gdListObj = JSON.parse(localStorage.getItem("u2_gd_list"))
+    if (gdListObj === null || gdListObj.date !== date) {
+        let gdListValue = getList()
+        gdListValue.then(g => {
+            gdListObj = { date: date, list: g.trim().split('\n') };
+            console.log(gdListObj)
+            localStorage.setItem("u2_gd_list", JSON.stringify(gdListObj)); // https://www.runoob.com/jsref/obj-storage.html
+        })
+        gdListValue.catch(e => {
+            console.log('下载列表发生错误，HTTP状态码[' + e + ']。');
+        })
+    }
+    const gdList = gdListObj.list;
     const lang = $('#locale_selection').val() // 获取当前网页使用的语言
     let txt1 = {
         'zh_CN': '发送请求',
         'zh_TW': '發送請求',
         'zh_HK': '發送請求',
-        'en_US': 'send request',
+        'en_US': 'Send request',
         'ru_RU': 'послать запрос' // 谷歌翻译
     }
     let txt2 = {
         'zh_CN': '做种良好',
         'zh_TW': '做種良好',
         'zh_HK': '做種良好',
-        'en_US': 'good seeding',
+        'en_US': 'Good seeding',
         'ru_RU': 'хороший посев' // 谷歌翻译
     }
     let txt3 = {
@@ -68,18 +77,19 @@
         'en_US': 'Email address is wrong or undefined, please re-enter.',
         'ru_RU': 'Адрес электронной почты неверен или не указан, введите его еще раз.' // 谷歌翻译
     }
-    const gdList = gdListObj.list;
-    const Uploaders = 7;
-    const userid = 45940; // 勿动
-    var GstaticIco;
-    const CurrentUrl = window.location.href;
-
-    if (CurrentUrl.indexOf("//u2.dmhy.org/torrents.php") != -1) {
+    let txt5 = {
+        'zh_CN': '下载种子',
+        'zh_TW': '下載種子',
+        'zh_HK': '下載種子',
+        'en_US': 'Download Torrent',
+        'ru_RU': 'Скачать Торрент'
+    }
+    if (location.href.match(/u2\.dmhy\.org\/torrents\.php/i)) {
         new torrents();
-    } else if (CurrentUrl.indexOf("//u2.dmhy.org/details.php") != -1 || CurrentUrl.indexOf("u2.dmhy.org/offers.php") != -1) {
+    } else if (location.href.match(/u2\.dmhy\.org\/(?:details|offers)\.php/i)) {
         new torrents();
         new details();
-    } else if (CurrentUrl.indexOf('//u2.dmhy.org/sendmessage.php?receiver=' + userid + '#') != -1) {
+    } else if (location.href.match(/u2\.dmhy\.org\/sendmessage\.php\?receiver=45940#\d+$/i)) { // 45940好像没法设置为变量
         new sendmessage();
     }
 
@@ -94,11 +104,11 @@
             if (Id_Data != -1 && SeederNum <= Uploaders) {
                 GstaticIco = $(this).find('td.embedded')[1];
                 $(GstaticIco).width(55);
-                $(GstaticIco).prepend('<a href="sendmessage.php?receiver=' + userid + '#' + id + '" target="_blank"><img src="bitbucket/drive_2020q4_48dp.png" style="padding-bottom: 2px; width:16px;height:16px;" alt="request" title="' + txt1[lang] + '"></a>');
+                $(GstaticIco).prepend('<a href="sendmessage.php?receiver=' + userid + '#' + id + '" target="_blank"><img src="//cdn.jsdelivr.net/gh/kysdm/u2_share@main/img/drive_2020q4_48dp.png" style="padding-bottom: 2px; width:16px;height:16px;" alt="request" title="' + txt1[lang] + '"></a>');
             } else if (Id_Data != -1) {
                 GstaticIco = $(this).find('td.embedded')[1];
                 $(GstaticIco).width(55);
-                $(GstaticIco).prepend('<img src="bitbucket/drive_2020q4_48dp.png" style="padding-bottom: 2px; width:16px;height:16px; filter: grayscale(100%);" alt="request" title="' + txt2[lang] + '">');
+                $(GstaticIco).prepend('<img src="//cdn.jsdelivr.net/gh/kysdm/u2_share@main/img/drive_2020q4_48dp.png" style="padding-bottom: 2px; width:16px;height:16px; filter: grayscale(100%);" alt="request" title="' + txt2[lang] + '">');
             }
         })
     }
@@ -117,7 +127,7 @@
     function sendmessage() {
         const email = $.cookie('gd_email');
         $("td.rowfollow > input[type=text]").val("#request#");
-        $(".bbcode").val('{ "id":"' + CurrentUrl.split("#")[1] + '" , "email":"' + email + '" }');
+        $(".bbcode").val('{ "id":"' + location.href.split("#")[1] + '" , "email":"' + email + '" }');
         $('td.toolbox').append('<input type="checkbox" name="save_email" checked="checked">' + txt3[lang] + '');
         $('#compose').attr('onsubmit', 'return checkemail();')
         $('body').append(
@@ -137,4 +147,47 @@ function checkemail(){\n\
 }</script>'
         )
     }
-})($);
+
+    function userdetails(msg) {
+        // https://developer.mozilla.org/zh-CN/docs/Web/API/DOMParser
+        let parser = new DOMParser();
+        let resXML = parser.parseFromString(msg, 'text/html');
+        const kaTable = resXML.querySelectorAll("body > table > tbody > tr:not(:first-child)")
+        for (var i = 0; i < kaTable.length; i++) {
+            let ka = kaTable[i].querySelector("td.rowfollow:nth-child(2) > table > tbody > tr:nth-child(1)")
+            let id = ka.querySelector("td > a").href.match(/id=(\d+)/i)[1]
+            let Id_Data = gdList.findIndex((value) => value == Number(id));
+            if (Id_Data != -1) { // 列表中没有做种人数信息，无法判断请求是否会被允许
+                $(ka).append(
+                    '<td width="40" class="embedded" style="text-align: right; width: 55px;" valign="middle">\
+        <a href="sendmessage.php?receiver=' + userid + '#' + id + '" target="_blank"><img src="//cdn.jsdelivr.net/gh/kysdm/u2_share@main/img/drive_2020q4_48dp.png" style="padding-bottom: 2px; width:16px;height:16px;filter: grayscale(55%);" alt="request" title="' + txt1[lang] + '"></a>\
+        <a href="download.php?id=' + id + '"><img class="download" src="pic/trans.gif" style="padding-bottom: 2px;" alt="download" title="' + txt5[lang] + '">\
+        </a></td>'
+                )
+            }
+        }
+        return resXML.body.innerHTML
+    }
+})();
+
+function getList() {
+    return new Promise((resolve, reject) => {
+        // https://www.w3school.com.cn/jquery/ajax_ajax.asp
+        $.ajax({
+            type: 'get',
+            url: 'https://cdn.jsdelivr.net/gh/kysdm/u2_share@main/u2list.txt',
+            contentType: 'text/plain',
+            dataType: 'text',
+            success: function (data, textStatus) {
+                resolve(data)
+                // console.log(data) // 返回字符串
+                // console.log(textStatus) // success
+            },
+            error: function (XMLHttpRequest, textStatus) {
+                reject(XMLHttpRequest.status)
+                // console.log(XMLHttpRequest) // xml对象
+                // console.log(textStatus) // error
+            }
+        })
+    })
+}
