f657c23e4ab06ec99c39e15cb124df5e9428b1f2
kysdm <gxk7231@gmail.com>
2021-03-16 19:10:24 +0800

    update

--- u2share_download_free_torrents.py
+++ u2share_download_free_torrents.py
@@ -11,8 +11,6 @@ import json
 import logging
 from bs4 import BeautifulSoup
 from logging import Handler, FileHandler, StreamHandler
-
-
 """
 姑且改了个出来。
 脚本本来是为了全站备份瞎写的，现在改成工具人脚本。
@@ -23,15 +21,18 @@ from logging import Handler, FileHandler, StreamHandler
 """
 
 # **************用户变量********************
-cookie = '' # u2 账户的 cookie
-passkey = '' # u2 账户的 passkey
+cookie = ''  # u2 账户的 cookie
+passkey = ''  # u2 账户的 passkey
 upLimit = '52219084'  # 上传限速
-qbthost = '127.0.0.1:8080' # qb 的 web 地址
-qbtusername = 'admin' # qb 的 web 登陆账户
-qbtpassword = '' # qb 的 web 登陆密码
+qbthost = '127.0.0.1:8080'  # qb 的 web 地址
+qbtusername = 'admin'  # qb 的 web 登陆账户
+qbtpassword = ''  # qb 的 web 登陆密码
 username = ''  # u2 账户的用户名，如果用户名有斜体，加入 <i></i>  懒得写自动获取了
 savepath = '/mnt/virtual/qbt/'  # 保存下载文件的路径
-proxies = {"http": "http://127.0.0.1:7890","https": "http://127.0.0.1:7890",} # 默认不使用代理访问，没人会在本地跑这个脚本的吧。
+proxies = {
+    "http": "http://127.0.0.1:7890",
+    "https": "http://127.0.0.1:7890",
+}  # 默认不使用代理访问，没人会在本地跑这个脚本的吧。
 # **************用户变量********************
 
 # **************变量********************
@@ -40,6 +41,7 @@ logfile = f'''{time.strftime('%Y%m%d',time.localtime(time.time()))}.log'''
 logpath = f'{abs_path}/log/自动下FREE'
 useragent = 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
 # **************变量********************
+
 
 class PathFileHandler(FileHandler):
     def __init__(self, path, filename, mode='a', encoding=None, delay=False):
@@ -60,12 +62,20 @@ class PathFileHandler(FileHandler):
 class Loggers(object):
     # 日志级别关系映射
     level_relations = {
-        'debug': logging.DEBUG, 'info': logging.INFO, 'warning': logging.WARNING,
-        'error': logging.ERROR, 'critical': logging.CRITICAL
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+        'critical': logging.CRITICAL
     }
 
-    def __init__(self, filename=logfile, level='info', log_dir=logpath,
-                 fmt='%(asctime)s - [line:%(lineno)d] - %(levelname)s: %(message)s'):
+    def __init__(
+            self,
+            filename=logfile,
+            level='info',
+            log_dir=logpath,
+            fmt='%(asctime)s - [line:%(lineno)d] - %(levelname)s: %(message)s'
+    ):
         self.logger = logging.getLogger(filename)
         abspath = os.path.dirname(os.path.abspath(__file__))
         self.directory = os.path.join(abspath, log_dir)
@@ -73,8 +83,9 @@ class Loggers(object):
         self.logger.setLevel(self.level_relations.get(level))  # 设置日志级别
         stream_handler = logging.StreamHandler()  # 往屏幕上输出
         stream_handler.setFormatter(format_str)
-        file_handler = PathFileHandler(
-            path=self.directory, filename=filename, mode='a')
+        file_handler = PathFileHandler(path=self.directory,
+                                       filename=filename,
+                                       mode='a')
         file_handler.setFormatter(format_str)
         self.logger.addHandler(stream_handler)
         self.logger.addHandler(file_handler)
@@ -99,11 +110,14 @@ def OpenHtml(url):
 def AnalysisMagic(html):
     soup = BeautifulSoup(html, 'lxml')
     # twoup_bg = soup.find_all('tr', attrs={'class': 'twoup_bg'}) # 2x 上传 1x 下载
-    twoupfree_bg = soup.find_all('tr', attrs={'class': 'twoupfree_bg'})  # 2x 上传 0x 下载
+    twoupfree_bg = soup.find_all('tr', attrs={'class':
+                                              'twoupfree_bg'})  # 2x 上传 0x 下载
     Magic(twoupfree_bg)
     free_bg = soup.find_all('tr', attrs={'class': 'free_bg'})  # 1x 上传 0x 下载
     Magic(free_bg)
-    custompromotion_bg = soup.find_all('tr', attrs={'class': 'custompromotion_bg'})  # ?x 上传 ?x 下载
+    custompromotion_bg = soup.find_all('tr',
+                                       attrs={'class': 'custompromotion_bg'
+                                              })  # ?x 上传 ?x 下载
     Magic(custompromotion_bg)
 
 
@@ -112,16 +126,17 @@ def Magic(var):
         v = str(v)
         if '<b>有效</b>' in v:
             if '全局' not in v and username not in v:  # 排除全局魔法 自己创建的魔法
-                re1 = re.compile(
-                    r'<a.+?href="promotion\.php.+?>(\d+?)<\/a>', re.S)
+                re1 = re.compile(r'<a.+?href="promotion\.php.+?>(\d+?)<\/a>',
+                                 re.S)
                 re2 = re.compile(r'<a href="details\.php\?id=(\d+?)">', re.S)
                 re3 = re.compile(
-                    r'<span title="&lt;time&gt;(\d{4}-\d{2}-\d{2}\s?\d{1,2}:\d{1,2}:\d{1,2})&lt;\/time&gt;">(.+?)<\/span>', re.S)
+                    r'<span title="&lt;time&gt;(\d{4}-\d{2}-\d{2}\s?\d{1,2}:\d{1,2}:\d{1,2})&lt;\/time&gt;">(.+?)<\/span>',
+                    re.S)
                 if 'alt="Promotion"' in v:  # 自定义魔法
                     re4 = re.compile(r'<img alt="上传比率".+?><b>(.+?)</b>', re.S)
                     re5 = re.compile(r'<img alt="下载比率".+?><b>(.+?)</b>', re.S)
-                    matchObj = re.match(
-                        r'<img\s?alt="下载比率".+?><b>(.+?)<\/b>', v, re.S)
+                    matchObj = re.match(r'<img\s?alt="下载比率".+?><b>(.+?)<\/b>',
+                                        v, re.S)
                     if matchObj:
                         if not '0.00X' == matchObj[0]:  # 排除下载倍率不是0的魔法   淦
                             log.info('非0魔法')
@@ -142,7 +157,9 @@ def Magic(var):
                         Magic_DO = '1.00X'
                     if Magic_DO == '0.00X':
                         Torrent_ID_List.append(Torrent_ID)
-                    log.info(f'{Magic_UP}↑ {Magic_DO}↓ ==> 魔法的ID:{Magic_ID}，种子的ID：{Torrent_ID}，魔法结束时间：{Magic_Start_Time}，魔法的时长：{Magic_Time}')
+                    log.info(
+                        f'{Magic_UP}↑ {Magic_DO}↓ ==> 魔法的ID:{Magic_ID}，种子的ID：{Torrent_ID}，魔法结束时间：{Magic_Start_Time}，魔法的时长：{Magic_Time}'
+                    )
 
                 if 'alt="FREE"' in v:  # 1x 上传 0x 下载
                     Magic_ID = (re.findall(re1, v))[0]
@@ -150,8 +167,10 @@ def Magic(var):
                     Magic_Start_Time = (re.findall(re3, v))[0][0]
                     Magic_Time = (re.findall(re3, v))[0][1]
                     Torrent_ID_List.append(Torrent_ID)
-                    log.info(f'1.00X↑ 0.00X↓ ==> 魔法的ID:{Magic_ID}，种子的ID：{Torrent_ID}，魔法结束时间：{Magic_Start_Time}，魔法的时长：{Magic_Time}')
-                elif 'alt="2X"' in v:    # 2x 上传 1x 下载
+                    log.info(
+                        f'1.00X↑ 0.00X↓ ==> 魔法的ID:{Magic_ID}，种子的ID：{Torrent_ID}，魔法结束时间：{Magic_Start_Time}，魔法的时长：{Magic_Time}'
+                    )
+                elif 'alt="2X"' in v:  # 2x 上传 1x 下载
                     pass
                 elif 'alt="2X Free"' in v:  # 2x 上传 0x 下载
                     Magic_ID = (re.findall(re1, v))[0]
@@ -159,7 +178,9 @@ def Magic(var):
                     Magic_Start_Time = (re.findall(re3, v))[0][0]
                     Magic_Time = (re.findall(re3, v))[0][1]
                     Torrent_ID_List.append(Torrent_ID)
-                    log.info(f'0.00X↑ 0.00X↓ ==> 魔法的ID:{Magic_ID}，种子的ID：{Torrent_ID}，魔法结束时间：{Magic_Start_Time}，魔法的时长：{Magic_Time}')
+                    log.info(
+                        f'0.00X↑ 0.00X↓ ==> 魔法的ID:{Magic_ID}，种子的ID：{Torrent_ID}，魔法结束时间：{Magic_Start_Time}，魔法的时长：{Magic_Time}'
+                    )
 
 
 class qbittorrent:
@@ -168,7 +189,8 @@ class qbittorrent:
             'user-agent': useragent,
             'Origin': f'http://{qbthost}',
             'Referer': f'http://{qbthost}/',
-            'Host': qbthost}
+            'Host': qbthost
+        }
         data = {'username': qbtusername, 'password': qbtpassword}
         url = f'http://{qbthost}/api/v2/auth/login'
         f = requests.post(url, data=data, headers=headers)
@@ -179,9 +201,7 @@ class qbittorrent:
             exit(1)
 
     def GetHash(self, sid, _id):
-        headers = {
-            'user-agent': useragent,
-            'cookie': f'SID={sid}'}
+        headers = {'user-agent': useragent, 'cookie': f'SID={sid}'}
         url = f'http://{qbthost}/api/v2/torrents/info'
         parameters = {'category': _id}
         f = requests.get(url, headers=headers, params=parameters)
@@ -193,9 +213,7 @@ class qbittorrent:
             exit(1)
 
     def Getinfo(self, sid, _filter):
-        headers = {
-            'user-agent': useragent,
-            'cookie': f'SID={sid}'}
+        headers = {'user-agent': useragent, 'cookie': f'SID={sid}'}
         url = f'http://{qbthost}/api/v2/torrents/info?filter={_filter}'
         f = requests.get(url, headers=headers)
         if f.status_code == 200:
@@ -203,8 +221,9 @@ class qbittorrent:
             jsonobj = json.loads(jsonre)
             __infolist = []
             for v in jsonobj:
-                __infolist.append(
-                    [v['hash'], v['category'], v['save_path'], v['time_active']])
+                __infolist.append([
+                    v['hash'], v['category'], v['save_path'], v['time_active']
+                ])
             return __infolist
         else:
             log.debug('无法获取种子info')
@@ -215,26 +234,31 @@ class qbittorrent:
             'cache-control': 'no-cache',
             'pragma': 'no-cache',
             'user-agent': useragent,
-            'cookie': f'SID={sid}'}
+            'cookie': f'SID={sid}'
+        }
         url = f'https://u2.dmhy.org/download.php?id={_id}&passkey={passkey}&https=1'
         # 下载种子文件
         r = requests.get(url)
         if r.status_code != 200:
             return r.status_code
 
-        files = {'file': (f'{_id}.torrent', r.content,
-                          'application/x-bittorrent')}
-        data = {'savepath': _savepath,
-                'category': category,
-                'autoTMM': 'false',
-                'paused': 'false',
-                'skip_checking': 'false',
-                'root_folder': 'true',
-                'dlLimit': 'NaN',
-                'upLimit': upLimit
-                }
-        f = requests.post(
-            f'http://{qbthost}/api/v2/torrents/add', data=data, headers=headers, files=files)
+        files = {
+            'file': (f'{_id}.torrent', r.content, 'application/x-bittorrent')
+        }
+        data = {
+            'savepath': _savepath,
+            'category': category,
+            'autoTMM': 'false',
+            'paused': 'false',
+            'skip_checking': 'false',
+            'root_folder': 'true',
+            'dlLimit': 'NaN',
+            'upLimit': upLimit
+        }
+        f = requests.post(f'http://{qbthost}/api/v2/torrents/add',
+                          data=data,
+                          headers=headers,
+                          files=files)
         if f.status_code == 200:
             return 200
         else:
@@ -242,29 +266,28 @@ class qbittorrent:
             exit(1)
 
     def DeleteTorrents(self, sid, _hash):
-        headers = {
-            'user-agent': useragent,
-            'cookie': f'SID={sid}'}
-        data = {'hashes': _hash,
-                'deleteFiles': 'false',
-                }
+        headers = {'user-agent': useragent, 'cookie': f'SID={sid}'}
+        data = {
+            'hashes': _hash,
+            'deleteFiles': 'false',
+        }
         url = f'http://{qbthost}/api/v2/torrents/delete'
         f = requests.post(url, headers=headers, data=data)
         return f.status_code
 
     def SetForceStart(self, sid, _hash):
-        headers = {
-            'user-agent': useragent,
-            'cookie': f'SID={sid}'}
+        headers = {'user-agent': useragent, 'cookie': f'SID={sid}'}
         url = f'http://{qbthost}/api/v2/torrents/setForceStart'
-        data = {'hashes': _hash,
-                'value': 'true'}
+        data = {'hashes': _hash, 'value': 'true'}
         f = requests.post(url, data=data, headers=headers)
         return f.status_code
 
 
 def Savecsv(_id):
-    with open(f'{abs_path}/FreeIdList.csv', 'a', newline='', encoding='utf-8-sig') as csv_file:
+    with open(f'{abs_path}/FreeIdList.csv',
+              'a',
+              newline='',
+              encoding='utf-8-sig') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow([_id])
     csv_file.close()
@@ -275,12 +298,15 @@ if __name__ == "__main__":
     Torrent_ID_List = []
     QBT = qbittorrent()
     log = Loggers(level='debug').logger
-    AnalysisMagic(OpenHtml('https://u2.dmhy.org/promotion.php?action=list&page=0'))
-    AnalysisMagic(OpenHtml('https://u2.dmhy.org/promotion.php?action=list&page=1'))
+    AnalysisMagic(
+        OpenHtml('https://u2.dmhy.org/promotion.php?action=list&page=0'))
+    AnalysisMagic(
+        OpenHtml('https://u2.dmhy.org/promotion.php?action=list&page=1'))
     # AnalysisMagic(OpenHtml('https://u2.dmhy.org/promotion.php?action=list&page=2'))
 
     if os.path.exists(f'{abs_path}/FreeIdList.csv'):
-        with open(f'{abs_path}/FreeIdList.csv', "r", encoding='utf-8-sig') as csv_file:
+        with open(f'{abs_path}/FreeIdList.csv', "r",
+                  encoding='utf-8-sig') as csv_file:
             reader = csv.reader(csv_file)
             for string in reader:
                 _IdList.append(string[0])
@@ -298,12 +324,13 @@ if __name__ == "__main__":
         else:
             save_path = f'{savepath}/{torrent_id}'
             log.info(f'{save_path}')
-            if QBT.AddTorrent(QBT.Login(), torrent_id, f'{torrent_id}', save_path) == 200:
+            if QBT.AddTorrent(QBT.Login(), torrent_id, f'{torrent_id}',
+                              save_path) == 200:
                 log.info('种子成功添加！开始下载...')
                 time.sleep(10)
                 _hash = QBT.GetHash(QBT.Login(), f'{torrent_id}')
                 if QBT.SetForceStart(QBT.Login(), _hash) == 200:
-                    Savecsv(torrent_id) # 写入数据库，防止重复下载
+                    Savecsv(torrent_id)  # 写入数据库，防止重复下载
                     log.info('启动下载成功！')
                 else:
                     log.debug('启动下载失败！')
