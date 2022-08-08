ae2cfae1b5d0051d94aa4b6048c546443bc47a6a
kysdm <gxk7231@gmail.com>
2022-08-08 21:25:18 +0800

    重构

--- u2share_auto_magic_it.py
+++ u2share_auto_magic_it.py
@@ -1,202 +1,236 @@
 # -*- coding: utf-8 -*-
 # by kysdm
 
+import os
+import sys
 import re
 import json
-import requests
-from time import sleep
+import logging
+import asyncio
+import aiohttp
+import datetime
+from lxml import etree
+from loguru import logger
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+'''
+Python3.9.9
+lxml==4.7.1
+apscheduler==3.6.3
+aiohttp==3.8.1
+loguru==0.5.3
+'''
 
-uid = 00000  # 自己的UID
-Upload = 1.00  # 魔法上传倍率
-Download = 0.00  # 魔法下载倍率
-Hours = 24  # 魔法时长
-Range = 'SELF'  # ALL 是地图炮 | SELF 是恢复系
-cookie = '__cfduid=; nexusphp_u2='  # cookie值
-proxies = {
-    "http": "",
-    "https": ""
-}  # 代理服务器 http://127.0.0.1:1088
-useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
-
-
-def GetHtml(url):
-    headers = {'user-agent': useragent, 'cookie': cookie}
-    i = 1
-    while i <= 15:
-        try:
-            html = requests.get(url,
-                                headers=headers,
-                                proxies=proxies,
-                                timeout=10)
-            if html.status_code < 400:
-                return html.text
-        except Exception:
-            print(f'打开网页发生错误！* {i} | {i * 5}秒后重试...')
-            sleep(i * 5)
-            i += 1
+INTERVAL = 60  # 周期性循环间隔 单位秒
+UID = 45940  # 自己的UID
+UPLOAD = 1.30  # 魔法上传倍率 | 上传比率范围1.3~2.33
+DOWNLOAD = 0.80  # 魔法下载倍率 | 下载比率范围0~0.8
+HOURS = 24  # 魔法时长  最低24小时
+RANGE = 'ALL'  # ALL 是地图炮 | SELF 是恢复系
+COMMENT_SWITCH = False  # True 备注消耗魔法数量 | False 反之
+DELAY = 0  # 魔法生效时间延迟x秒 适当延迟地图炮魔法生效时间，魔法消息不再显示在群聊区
+COOKIE = {'nexusphp_u2': 'xxx'}  # cookie值
+PROXIES = ''  # 代理服务器 PROXIES="http://user:pass@some.proxy.com"
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
 
 
-def tlist():
-    print('开始下载列表... (如果报错，代表连接U2失败)\n')
-    html = GetHtml(
-        f'https://u2.dmhy.org/getusertorrentlistajax.php?userid={uid}&type=leeching'
-    )
-    _h1 = re.findall(
-        r'<tr><td class="rowfollow nowrap".*?>(?:---</td></tr>|\d+?\.\d+?</font></td></tr>)',
-        html, re.S | re.I)
-    _Magic_List = []
+class U2():
+
+    def __init__(self):
+        self.session = None
+
+    async def get(self, url: str) -> dict:
+        if self.session is None:
+            self.session = aiohttp.ClientSession()
+        for i in range(15):
+            try:
+                timeout = aiohttp.ClientTimeout(total=80, sock_connect=15, sock_read=45)
+                async with self.session.get(url, headers=HEADERS, cookies=COOKIE, proxy=PROXIES, timeout=timeout) as resp:
+                    if resp.status == 200:
+                        text = await resp.text()
+                        return etree.HTML(text)
+                    else:
+                        logger.error(f'状态码不正确<{resp.status}> * {i} | 10秒后重试... | {url}')
+                        await asyncio.sleep(10)
+            except asyncio.TimeoutError as e:
+                logger.error(f'超时 * {i} | 10秒后重试... | {url} | {e}')
+                await asyncio.sleep(10)
+            except Exception as e:
+                logger.error(f'发生致命错误 | 30秒后重试... | {url} | {e}')
+                await asyncio.sleep(30)
+
+    async def post(self, url, data):
+        if self.session is None:
+            self.session = aiohttp.ClientSession()
+        for i in range(15):
+            try:
+                timeout = aiohttp.ClientTimeout(total=80, sock_connect=15, sock_read=45)
+                async with self.session.post(url, data=data, headers=HEADERS, cookies=COOKIE, proxy=PROXIES, timeout=timeout) as resp:
+                    if resp.status == 200:
+                        return await resp.text()
+                    else:
+                        logger.error(f'状态码不正确<{resp.status}> * {i}  | 10秒后重试... | {url}')
+                        await asyncio.sleep(10)
+            except asyncio.TimeoutError as e:
+                logger.error(f'超时 * {i} | 10秒后重试... | {url} | {e}')
+                await asyncio.sleep(10)
+            except Exception as e:
+                logger.error(f'发生致命错误 | 30秒后重试... | {url} | {e}')
+                await asyncio.sleep(30)
+
+    async def close(self) -> None:
+        if self.session is not None:
+            await self.session.close()
+
+
+async def getUserTorrentList():
+    logger.info('开始下载列表... (如果报错，代表连接U2失败)')
+    html = await u2.get(f'https://u2.dmhy.org/getusertorrentlistajax.php?userid={UID}&type=leeching')
+    # _h1 = re.findall(r'<tr><td class="rowfollow nowrap".*?>(?:---</td></tr>|\d+?\.\d+?</font></td></tr>)', html, re.S | re.I)
+    _h1 = html.xpath('/html/body/table/tr')
+    _h1.pop(0)
+    __list = []
+
     for each in _h1:
-        # print(each)
-        _id = re.search(r'<a href="details.php\?id=(\d+?)&hit=1">', each,
-                        re.M | re.I).group(1)  # 种子ID
-        seeders = re.search(
-            r'<a href="details.php\?id=\d+?&amp;hit=1&amp;dllist=1#seeders">(\d+?)</a>',
-            each, re.M | re.I).group(1)  # 做种人数
-        pro_free2up_search = re.search(
-            r'<img class="pro_free2up" src="pic/trans.gif" alt="2X Free"(?:\s/)?>',
-            each, re.M | re.I)
-        pro_custom_search = re.search(
-            r'<img class="arrowup" src="pic/trans.gif" alt="上传比率"(?:\s/)?><b>(.+?)X</b>&nbsp;<img class="arrowdown" src="pic/trans.gif" alt="下载比率"(?:\s/)?><b>(.+?)X</b>',
-            each, re.M | re.I)
-        pro_50pctdown2up_search = re.search(
-            r'<img class="pro_50pctdown2up" src="pic/trans.gif" alt="2X 50%"(?:\s/)?>',
-            each, re.M | re.I)
-        pro_2up_search = re.search(
-            r'<img class="pro_2up" src="pic/trans.gif" alt="2X"(?:\s/)?>',
-            each, re.M | re.I)
-        pro_50pctdown_search = re.search(
-            r'<img class="pro_50pctdown" src="pic/trans.gif" alt="50%"(?:\s/)?>',
-            each, re.M | re.I)
-        pro_30pctdown_search = re.search(
-            r'<img class="pro_30pctdown" src="pic/trans.gif" alt="30%"(?:\s/)?>',
-            each, re.M | re.I)
-        pro_free_search = re.search(
-            r'<img class="pro_free" src="pic/trans.gif" alt="FREE"(?:\s/)?>',
-            each, re.M | re.I)
+        # x = etree.tostring(each, encoding='utf-8').decode('utf-8')
+        _id = re.search(r'id=(\d+)', each.xpath('.//*[@class="torrentname"]/.//a/@href')[0], flags=re.I).group(1)  # 种子ID
+        # y = each.xpath('.//*[contains(@href,"#seeders")]/text()')[0]
+        seeders = each.xpath('.//a[contains(@href,"#seeders")]/text()')[0]  # 做种人数
 
-        if pro_free2up_search:  # 2倍上传 免费下载 img.pro_free2up
-            _Magic_List.append([_id, seeders, '2.00 / 0.00'])
-        elif pro_custom_search:  # 2倍上传 30%下载 img.pro_custom
-            _Magic_List.append([
-                _id, seeders,
-                f'{pro_custom_search.group(1)} / {pro_custom_search.group(2)}'
-            ])
-        elif pro_50pctdown2up_search:  # 2倍上传 50%下载 img.pro_50pctdown2up
-            _Magic_List.append([_id, seeders, '2.00 / 0.50'])
-        elif pro_2up_search:  # 2倍上传 img.pro_2up
-            _Magic_List.append([_id, seeders, '2.00 / 1.00'])
-        elif pro_50pctdown_search:  # 50%下载 pro_50pctdown
-            _Magic_List.append([_id, seeders, '1.00 / 0.50'])
-        elif pro_30pctdown_search:  # 30%下载 img.pro_30pctdown
-            _Magic_List.append([_id, seeders, '1.00 / 0.30'])
-        elif pro_free_search:  # 免费下载 img.pro_free
-            _Magic_List.append([_id, seeders, '1.00 / 0.00'])
+        if len(each.xpath('.//img[@class="pro_free2up"]')) != 0:
+            # 2x 0x
+            __list.append([_id, seeders, '2.00 / 0.00'])
+        elif len(each.xpath('.//img[@class="pro_free"]')) != 0:
+            # 1x 0x
+            __list.append([_id, seeders, '1.00 / 0.00'])
+        elif len(each.xpath('.//img[@class="pro_2up"]')) != 0:
+            # 2x 1x
+            __list.append([_id, seeders, '2.00 / 1.00'])
+        elif len(each.xpath('.//img[@class="pro_custom"]')) != 0:
+            # 自定义魔法
+            _u = each.xpath('.//img[@class="arrowup"]/following-sibling::b[1]')[0].xpath("string(.)").replace('X', '')
+            _d = each.xpath('.//img[@class="arrowdown"]/following-sibling::b[1]')[0].xpath("string(.)").replace('X', '')
+            __list.append([_id, seeders, f'{_u} / {_d}'])
+        elif len(each.xpath('.//img[@class="pro_50pctdown2up"]')) != 0:
+            # 2x 0.5x
+            __list.append([_id, seeders, '2.00 / 0.50'])
+        elif len(each.xpath('.//img[@class="pro_50pctdown"]')) != 0:
+            # 1x 0.5x
+            __list.append([_id, seeders, '1.00 / 0.50'])
+        elif len(each.xpath('.//img[@class="pro_30pctdown"]')) != 0:
+            # 1x 0.3x
+            __list.append([_id, seeders, '1.00 / 0.30'])
         else:
-            _Magic_List.append([_id, seeders, '1.00 / 1.00'])
+            # 没有魔法
+            __list.append([_id, seeders, '1.00 / 1.00'])
 
-    return sorted([list(t) for t in set(tuple(_) for _ in _Magic_List)], key=_Magic_List.index)
+    return sorted([list(t) for t in set(tuple(_) for _ in __list)], key=__list.index)
 
 
-def Send(_id, _ur, _dr):
-    html = GetHtml(
-        f'https://u2.dmhy.org/promotion.php?action=magic&torrent={_id}')
-    divergence = re.search(
-        r'<input type="hidden" name="divergence" value="(.+?)"(?:\s/)?>', html,
-        re.M | re.I).group(1)
-    base_everyone = re.search(
-        r'<input type="hidden" name="base_everyone" value="(.+?)"(?:\s/)?>',
-        html, re.M | re.I).group(1)
-    base_self = re.search(
-        r'<input type="hidden" name="base_self" value="(.+?)"(?:\s/)?>', html,
-        re.M | re.I).group(1)
-    base_other = re.search(
-        r'<input type="hidden" name="base_other" value="(.+?)"(?:\s/)?>', html,
-        re.M | re.I).group(1)
-    tsize = re.search(
-        r'<input type="hidden" name="tsize" value="(.+?)"(?:\s/)?>', html,
-        re.M | re.I).group(1)
-    ttl = re.search(r'<input type="hidden" name="ttl" value="(.+?)"(?:\s/)?>',
-                    html, re.M | re.I).group(1)
-    headers = {
-        'Referer':
-        f'https://u2.dmhy.org/promotion.php?action=magic&torrent={_id}',
-        'origin': 'https://u2.dmhy.org',
-        'User-Agent': useragent,
-        'cookie': cookie
-    }
+async def sendMagic(_id, _ur, _dr):
+    html = await u2.get(f'https://u2.dmhy.org/promotion.php?action=magic&torrent={_id}')
+
+    try:
+        _divergence = html.xpath('//input[@name="divergence"]/@value')[0]
+        _base_everyone = html.xpath('//input[@name="base_everyone"]/@value')[0]
+        _base_self = html.xpath('//input[@name="base_self"]/@value')[0]
+        _base_other = html.xpath('//input[@name="base_other"]/@value')[0]
+        _tsize = html.xpath('//input[@name="tsize"]/@value')[0]
+        _ttl = html.xpath('//input[@name="ttl"]/@value')[0]
+    except IndexError:
+        logger.error(f'#{_id} | 获取种子基础信息错误，可能种子已被屏蔽。')
+        return 404
+
     data = {
         'action': 'magic',
-        'divergence': divergence,  # 全站基数
-        'base_everyone': base_everyone,
-        'base_self': base_self,
-        'base_other': base_other,
+        'divergence': _divergence,  # 全站基数
+        'base_everyone': _base_everyone,
+        'base_self': _base_self,
+        'base_other': _base_other,
         'torrent': _id,  # 种子ID
-        'tsize': tsize,  # 种子大小
-        'ttl': ttl,  # 种子已存在时间
-        'user': Range,  # 为自己放魔法
+        'tsize': _tsize,  # 种子大小
+        'ttl': _ttl,  # 种子已存在时间
+        'user': RANGE,  # 为类型
         # 'user_other':'', # 为他人放魔法才有用
         'start': '0',  # 魔法立即生效
-        'hours': str(Hours),  # 魔法有效期24小时
+        'hours': str(HOURS if HOURS > 24 else 24),  # 魔法有效期24小时
         'promotion': '8',  # 魔法类型为其他
         'ur': _ur,  # 上传比率
         'dr': _dr,  # 下载比率
         'comment': ''  # 评论
     }
-    try:
-        f1 = requests.post('https://u2.dmhy.org/promotion.php?test=1',
-                           data=data,
-                           headers=headers,
-                           proxies=proxies,
-                           timeout=10)
-    except Exception:
-        return f1.status_code
-    JsonObj = json.loads(f1.text)
-    if not JsonObj['status'] == 'operational':
-        print(f'{_id} | 魔法不可用')
+
+    __j = await u2.post('https://u2.dmhy.org/promotion.php?test=1', data)
+
+    if __j is None:
+        return 503
+
+    __json = json.loads(__j)
+
+    if not __json['status'] == 'operational':
+        logger.warning(f'#{_id}  魔法不可用')
         return 403
-    uc = re.search(r'title="(.+?)"', JsonObj['price'],
-                   re.M | re.I).group(1).replace(',', '')
-    data['comment'] = f'消耗UCoin: {uc}'
-    sleep(3)
+
+    _uc = re.search(r'title="(.+?)"', __json['price'], re.M | re.I).group(1).replace(',', '')
+
+    if COMMENT_SWITCH is True:
+        data['comment'] = f'UCoin: {_uc}'
+
+    data['start'] = 0 if DELAY == 0 else (datetime.datetime.now() + datetime.timedelta(seconds=DELAY)).strftime('%Y-%m-%d %H:%M:%S')
+
+    await asyncio.sleep(3)
+
     try:
-        f2 = requests.post(
-            f'https://u2.dmhy.org/promotion.php?action=magic&torrent={_id}',
-            data=data,
-            headers=headers,
-            proxies=proxies,
-            timeout=10)
-    except Exception:
-        pass
-    finally:
-        return f2.status_code
+        f = await u2.post(f'https://u2.dmhy.org/promotion.php?action=magic&torrent={_id}', data=data)
+        return 200 if re.match(r'^<script.+<\/script>$', f) else 503
+    except Exception as e:
+        logger.error(f'#{_id} | 释放魔法发生错误 | {e}')
+        return 503
 
 
-def Magic():
-    _List = tlist()
-    for _id, _seeders, _magic in _List:
+async def main():
+    __list = await getUserTorrentList()
+
+    for _id, _seeders, _magic in __list:
+
         if _seeders == '0':
-            print(f'ID:{_id} | 无人做种暂不释放魔法')
+            logger.debug(f'ID:{_id} | 无人做种暂不释放魔法')
             continue
-        _Upload = float(_magic.split(' / ')[0])  # 上传
-        _Download = float(_magic.split(' / ')[1])  # 下载
-        if Upload <= _Upload and Download >= _Download:
-            print(f'ID:{_id} | 魔法已存在')
-            continue
-        if _Upload < Upload:
-            ur = str(Upload)
-        else:
-            ur = '1.00'
-        if _Download > Download:
-            dr = str(Download)
-        else:
-            dr = '1.00'
 
-        if Send(_id, ur, dr) == 200:
-            print(f'ID：{_id} | 成功施加马猴烧酒')
+        __upload = float(_magic.split(' / ')[0])  # 上传
+        __download = float(_magic.split(' / ')[1])  # 下载
+
+        if UPLOAD <= __upload and DOWNLOAD >= __download:
+            logger.info(f'ID:{_id} | 魔法已存在')
+            continue
+
+        ur = str(UPLOAD) if __upload < UPLOAD else '1.00'
+        dr = str(DOWNLOAD) if __download > DOWNLOAD else '1.00'
+
+        if await sendMagic(_id, ur, dr) == 200:
+            logger.info(f'ID:{_id} | 成功施加马猴烧酒')
         else:
-            print(f'ID：{_id} | 发送错误')
-        sleep(5)
+            logger.error(f'ID:{_id} | 发送错误')
+
+        await asyncio.sleep(1)
+
+    logger.info('END')
 
 
 if __name__ == "__main__":
-    Magic()
+    job_defaults = {'coalesce': True, 'max_instances': 1, 'misfire_grace_time': 60}
+    scheduler = AsyncIOScheduler(job_defaults=job_defaults, timezone='Asia/Shanghai')
+    logging.getLogger('apscheduler.scheduler').setLevel(level=logging.ERROR)
+    logger.remove()
+    logger.add(sink=sys.stdout, backtrace=True, diagnose=True, enqueue=True)
+    u2 = U2()
+
+    scheduler.add_job(func=main, trigger='interval', seconds=INTERVAL, next_run_time=datetime.datetime.now())
+    scheduler.start()
+
+    logger.info('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+    try:
+        loop_forever = asyncio.new_event_loop()
+        coro = asyncio.get_event_loop().run_forever()
+        future = asyncio.run_coroutine_threadsafe(coro, loop_forever)
+    except (KeyboardInterrupt, SystemExit):
+        pass
